cc0f9f568cbe3ccb83169ce8e3700b50e621ebd0
Freddie Sackur <freddie.git@dustyfox.uk>
2025-02-28 18:11:49 +0000

    explicitly add parameters to api (wip)

--- opnsense_cli/template_engines/jinja2.py
+++ opnsense_cli/template_engines/jinja2.py
@@ -1,8 +1,14 @@
+import re
 import os
 from jinja2 import Template, Environment, BaseLoader
 from opnsense_cli.template_engines.base import TemplateEngine
 from opnsense_cli.template_engines.exceptions import TemplateNotFoundException
 from jinja2.exceptions import TemplateNotFound, TemplatesNotFound
+
+
+# Custom filter method
+def regex_replace(s, find, replace):
+    return re.sub(find, replace, s)
 
 
 class Jinja2TemplateEngine(TemplateEngine):
@@ -17,7 +23,9 @@ class Jinja2TemplateEngine(TemplateEngine):
             raise TemplateNotFoundException(template_path)
 
     def set_template_from_string(self, template_str, **kwargs) -> Template:
-        self.template = Environment(loader=BaseLoader, **kwargs).from_string(template_str)
+        env = Environment(loader=BaseLoader, **kwargs)
+        env.filters["regex_replace"] = regex_replace
+        self.template = env.from_string(template_str)
 
     def render(self):
         return self.template.render(vars=self.vars)
